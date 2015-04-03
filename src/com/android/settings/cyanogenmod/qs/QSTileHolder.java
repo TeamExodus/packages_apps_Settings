0217c712007697f7141d5ddf5f6deb9879764427
Adrian Grimm <usmcamgrimm@gmail.com>
2015-04-02 21:42:18 -0400

    Hide EQ Tile [2/2]
    Change-Id: If30880470c33f422844a4210a47d30a9ad8ed61c

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -141,10 +141,10 @@ public class QSTileHolder {
 				resourceName = "ic_qs_update";
 				stringId = R.string.vanirupdate_settings_title;
 				break;
-            case QSConstants.TILE_VISUALIZER:
+            /* case QSConstants.TILE_VISUALIZER:
                 resourceName = "ic_qs_visualizer_static";
                 stringId = R.string.qs_tile_visualizer;
-                break;
+                break; */
             case QSConstants.TILE_SCREEN_TIMEOUT:
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
