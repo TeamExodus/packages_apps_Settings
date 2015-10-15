0c71a3faba0e3ac51d558a6cb82f13f9a72d3138
MartinRo <martin@rodemann.org>
2015-10-15 18:32:27 +0000

    Fix merging issue
    Change-Id: I63862f9256ffcb170fd0b860580a0bceac968553

--- src/com/android/settings/cyanogenmod/NavRing.java
+++ src/com/android/settings/cyanogenmod/NavRing.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
